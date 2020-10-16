4dcaafa48188e2a230e7f949fd3d8ade232d3313
Sean Blessing <snblessing@gmail.com>
2020-10-16 11:37:13 -0400

    final changes

--- src/main/java/com/example/demo/web/TestController.java
+++ src/main/java/com/example/demo/web/TestController.java
@@ -1,5 +1,8 @@
 package com.example.demo.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.business.Pumpkin;
@@ -7,6 +10,7 @@ import com.example.demo.business.Pumpkin;
 @RestController
 @RequestMapping("/tests")
 public class TestController {
+	private List<Pumpkin> pumpkins = new ArrayList<>();
 	
 	@GetMapping("/")
 	public String testing() {
@@ -18,4 +22,14 @@ public class TestController {
 		Pumpkin p = new Pumpkin(1,"orange",11.5, 12, 15, true);
 		return p;
 	}
+	
+	@GetMapping("/pumpkin-1")
+	public List<Pumpkin> makePumpkin(@RequestParam int id, @RequestParam String color,
+			@RequestParam double weight, @RequestParam double height, 
+			@RequestParam double width, @RequestParam boolean decorated) {
+		Pumpkin p = new Pumpkin(id, color, weight, height, width, decorated);
+		pumpkins.add(p);
+		return pumpkins;
+	}
+
 }
